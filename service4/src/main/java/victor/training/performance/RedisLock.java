c2e6b6a5bc5c0f0da997aba633c07fd0e88be68f
Victor Rentea <victorrentea@gmail.com>
2023-01-31 15:03:38 +0200

    Memory Leak under heavy load

--- service4/src/main/java/victor/training/performance/RedisLock.java
+++ service4/src/main/java/victor/training/performance/RedisLock.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ForkJoinPool;
 import java.util.concurrent.locks.Lock;
 
 import static java.lang.Thread.sleep;
@@ -15,7 +16,7 @@ import static java.util.concurrent.TimeUnit.SECONDS;
 
 
 class SomeData1KB {
-  int[] data = new int[1024/8];
+  int[] data = new int[1024000/8];
 }
 @Slf4j
 @RestController
@@ -26,24 +27,26 @@ public class RedisLock {
 
   @GetMapping("lock")
   public String properLock() throws InterruptedException {
+    SomeData1KB data = repoFindById();
+    CompletableFuture.runAsync(() ->longRunningProcess(data)); // fire and forget method on ForkJoinPool
+    // any thread pool has a queue. WHAT SIZE? idn ∞ ?!!
+
+    // bad practice: call a method async oassing a large object kept in mem until the func runs, without putting a queue max size,
+    return "Got your job ty!";
+  }
+  @SneakyThrows
+  private void longRunningProcess(SomeData1KB data)  {
     Lock lock;
     try {
       lock = lockRegistry.obtain(MY_LOCK_KEY);
     } catch (Exception e) {
       throw new RuntimeException("Cannot obtain lock: " + MY_LOCK_KEY, e);
     }
-    SomeData1KB data = repoFindById();
-    CompletableFuture.runAsync(() ->longRunningProcess(lock, data));
-
-    return "Got your job ty!";
-  }
-
-
-  @SneakyThrows
-  private static void longRunningProcess(Lock lock, SomeData1KB data)  {
     try {
       //  🛑 childish/initial parameters ~> tune timeouts considering load to avoid OOME ~>
-      if (lock.tryLock(2, SECONDS)) {
+      if (lock.tryLock(2, SECONDS)) { // how many threads at one point in time can be blocked here ?
+        // hint: my machine has 10 CPUs =>
+        // // TODO EXPECT 8 = (10-1)-1(that entered) - blocking the entire commonPool in JVM
         log.info("ENTER critical section");
 
         sleep(1000);
